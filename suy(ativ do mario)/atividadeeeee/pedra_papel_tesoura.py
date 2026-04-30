1808451d64bb8ae1cb053f49d71694f42cb0b4d6
suy77 <suellenquintela@gmail.com>
2026-04-30 16:22:06 -0300

    Update pedra_papel_tesoura.py

--- suy(ativ do mario)/atividadeeeee/pedra_papel_tesoura.py
+++ suy(ativ do mario)/atividadeeeee/pedra_papel_tesoura.py
@@ -1,4 +1,9 @@
+vc = input("escolha entre pedra, papel e tesoura: ")
 import random
 lista = ["pedra", "papel", "tesoura"]
 escolha = random.choice(lista)
 print(escolha)
+while true:
+    if vc == escolha:
+        print("voce perdeu")
+# eu vou surtarrrrrr
