67c88e9f02bb59a64e781c995039f0d310179f6e
suy77 <suellenquintela@gmail.com>
2026-05-08 13:02:43 -0300

    Update pedra_papel_tesoura.py

--- suy(ativ do mario)/atividadeeeee/pedra_papel_tesoura.py
+++ suy(ativ do mario)/atividadeeeee/pedra_papel_tesoura.py
@@ -1,24 +1,34 @@
 import random
 
+print("~~ joguinho de pedra, papel e tesoura :3 ~~")
 lista = ['papel', 'pedra', 'tesoura']
+jogador_pontos = []
+maquina_pontos = []
+
 while True:
-    jogador = (0)
-    maquina = (0)
-      jogador < 3 and maquina < 3:
+    jogador_pontos = 0
+    maquina_pontos = 0
+    jogador_pontos < 3 and maquina_pontos < 3 
 
-     jogador = input("Escolha entre pedra, papel e tesoura: ").lower()
-     maquina = random.choice(lista)
-     print("a maquina jogou: ",(maquina))
-
+    jogador = input("Escolha sua mão: ").lower()
     if jogador not in lista:
-        print("Essa opção não existe, amigo.")
+      print("Essa opção não existe, amigo.")
+      continue
+    maquina = random.choice(lista)
+    print("a maquina jogou: ",(maquina))
 
-    elif jogador == maquina:
+    if jogador == maquina:
         print("deu empate :P")
+        continue
 
-    if (jogador == 'pedra' and maquina == 'pappel') or \
+    if (jogador == 'pedra' and maquina == 'tesoura') or \
+       (jogador == 'papel' and maquina == 'pedra') or \
+       (jogador == 'tesoura' and maquina == 'papel'):
+       jogador = jogador_pontos =+ 1
+       print(f"voce ganhouuuu 1 ponto :D. total --> {jogador}")
+
+    else:
+       maquina = maquina_pontos =+ 1
+       print(f"a maquina ganhouuuu. total dela --> {maquina}")
+
     
-
-    #else:
-     #   print("ponto pra maquina")
-      #  maquina = maquina+1
